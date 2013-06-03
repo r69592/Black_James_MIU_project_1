8034e50ad296253563fb14400fedfb80b9df60ee
JamesOBlack <jamesoblack@fullsail.edu>
2013-06-03 16:16:21 -0400

    Updating apperance.

--- js/json.js
+++ js/json.js
@@ -14,5 +14,149 @@ var json = {
 		"importance": ["Importance:", "10"],
 		"choreDate":  ["Chore Date:", "2013-05-23"],
 		"notes":      ["Notes:", "Test 2."] 
+	},
+	"chore3": {
+		"chore":      ["Chore:", "Laundry"],
+		"area":       ["Area:", "laundry"],
+		"difficulty": ["Difficulty:", "Hard"],
+		"importance": ["Importance:", "5"],
+		"choreDate":  ["Chore Date:", "2013-06-03"],
+		"notes":      ["Notes:", "Test 1."] 
+	},
+	"chore4": {
+		"chore":      ["Chore:", "Dishes"],
+		"area":       ["Area:", "kitchen"],
+		"difficulty": ["Difficulty:", "Hard"],
+		"importance": ["Importance:", "10"],
+		"choreDate":  ["Chore Date:", "2013-05-23"],
+		"notes":      ["Notes:", "Test 2."] 
+	},
+	"chore5": {
+		"chore":      ["Chore:", "Grass"],
+		"area":       ["Area:", "yard"],
+		"difficulty": ["Difficulty:", "Hard"],
+		"importance": ["Importance:", "5"],
+		"choreDate":  ["Chore Date:", "2013-05-23"],
+		"notes":      ["Notes:", "Test 1."] 
+	},
+	"chore6": {
+		"chore":      ["Chore:", "Dishes"],
+		"area":       ["Area:", "kitchen"],
+		"difficulty": ["Difficulty:", "Hard"],
+		"importance": ["Importance:", "10"],
+		"choreDate":  ["Chore Date:", "2013-05-23"],
+		"notes":      ["Notes:", "Test 2."] 
+	},
+	"chore7": {
+		"chore":      ["Chore:", "Grass"],
+		"area":       ["Area:", "yard"],
+		"difficulty": ["Difficulty:", "Hard"],
+		"importance": ["Importance:", "5"],
+		"choreDate":  ["Chore Date:", "2013-05-23"],
+		"notes":      ["Notes:", "Test 1."] 
+	},
+	"chore8": {
+		"chore":      ["Chore:", "Dishes"],
+		"area":       ["Area:", "kitchen"],
+		"difficulty": ["Difficulty:", "Hard"],
+		"importance": ["Importance:", "10"],
+		"choreDate":  ["Chore Date:", "2013-05-23"],
+		"notes":      ["Notes:", "Test 2."] 
+	},
+	"chore9": {
+		"chore":      ["Chore:", "Grass"],
+		"area":       ["Area:", "yard"],
+		"difficulty": ["Difficulty:", "Hard"],
+		"importance": ["Importance:", "5"],
+		"choreDate":  ["Chore Date:", "2013-05-23"],
+		"notes":      ["Notes:", "Test 1."] 
+	},
+	"chore10": {
+		"chore":      ["Chore:", "Dishes"],
+		"area":       ["Area:", "kitchen"],
+		"difficulty": ["Difficulty:", "Hard"],
+		"importance": ["Importance:", "10"],
+		"choreDate":  ["Chore Date:", "2013-05-23"],
+		"notes":      ["Notes:", "Test 2."] 
+	},
+	"chore11": {
+		"chore":      ["Chore:", "Grass"],
+		"area":       ["Area:", "yard"],
+		"difficulty": ["Difficulty:", "Hard"],
+		"importance": ["Importance:", "5"],
+		"choreDate":  ["Chore Date:", "2013-05-23"],
+		"notes":      ["Notes:", "Test 1."] 
+	},
+	"chore12": {
+		"chore":      ["Chore:", "Dishes"],
+		"area":       ["Area:", "kitchen"],
+		"difficulty": ["Difficulty:", "Hard"],
+		"importance": ["Importance:", "10"],
+		"choreDate":  ["Chore Date:", "2013-05-23"],
+		"notes":      ["Notes:", "Test 2."] 
+	},
+	"chore13": {
+		"chore":      ["Chore:", "Grass"],
+		"area":       ["Area:", "yard"],
+		"difficulty": ["Difficulty:", "Hard"],
+		"importance": ["Importance:", "5"],
+		"choreDate":  ["Chore Date:", "2013-05-23"],
+		"notes":      ["Notes:", "Test 1."] 
+	},
+	"chore14": {
+		"chore":      ["Chore:", "Dishes"],
+		"area":       ["Area:", "kitchen"],
+		"difficulty": ["Difficulty:", "Hard"],
+		"importance": ["Importance:", "10"],
+		"choreDate":  ["Chore Date:", "2013-05-23"],
+		"notes":      ["Notes:", "Test 2."] 
+	},
+	"chore15": {
+		"chore":      ["Chore:", "Grass"],
+		"area":       ["Area:", "yard"],
+		"difficulty": ["Difficulty:", "Hard"],
+		"importance": ["Importance:", "5"],
+		"choreDate":  ["Chore Date:", "2013-05-23"],
+		"notes":      ["Notes:", "Test 1."] 
+	},
+	"chore16": {
+		"chore":      ["Chore:", "Dishes"],
+		"area":       ["Area:", "kitchen"],
+		"difficulty": ["Difficulty:", "Hard"],
+		"importance": ["Importance:", "10"],
+		"choreDate":  ["Chore Date:", "2013-05-23"],
+		"notes":      ["Notes:", "Test 2."] 
+	},
+	"chore17": {
+		"chore":      ["Chore:", "Grass"],
+		"area":       ["Area:", "yard"],
+		"difficulty": ["Difficulty:", "Hard"],
+		"importance": ["Importance:", "5"],
+		"choreDate":  ["Chore Date:", "2013-05-23"],
+		"notes":      ["Notes:", "Test 1."] 
+	},
+	"chore18": {
+		"chore":      ["Chore:", "Dishes"],
+		"area":       ["Area:", "kitchen"],
+		"difficulty": ["Difficulty:", "Hard"],
+		"importance": ["Importance:", "10"],
+		"choreDate":  ["Chore Date:", "2013-05-23"],
+		"notes":      ["Notes:", "Test 2."] 
+	},
+	"chore19": {
+		"chore":      ["Chore:", "Grass"],
+		"area":       ["Area:", "yard"],
+		"difficulty": ["Difficulty:", "Hard"],
+		"importance": ["Importance:", "5"],
+		"choreDate":  ["Chore Date:", "2013-05-23"],
+		"notes":      ["Notes:", "Test 1."] 
+	},
+	"chore20": {
+		"chore":      ["Chore:", "Dishes"],
+		"area":       ["Area:", "kitchen"],
+		"difficulty": ["Difficulty:", "Hard"],
+		"importance": ["Importance:", "10"],
+		"choreDate":  ["Chore Date:", "2013-05-23"],
+		"notes":      ["Notes:", "Test 2."] 
 	}
 }
